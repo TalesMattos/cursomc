3aa51283ad33b238becb0110eab8ae96ff133347
TalesMattos <mattos.tales@gmail.com>
2018-10-02 18:45:34 -0300

    identação

--- src/main/java/com/tales/cursomc/CursomcApplication.java
+++ src/main/java/com/tales/cursomc/CursomcApplication.java
@@ -15,17 +15,17 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
+
 	}
 }
