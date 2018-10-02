d7f54ace8a92dc4d19323daee89c580a05b67687
TalesMattos <mattos.tales@gmail.com>
2018-10-02 18:47:29 -0300

    identação

--- src/main/java/com/tales/cursomc/domain/Categoria.java
+++ src/main/java/com/tales/cursomc/domain/Categoria.java
@@ -12,16 +12,17 @@ import javax.persistence.Id;
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column
 	private String nome;
-	
-	public Categoria() {}
-	
+
+	public Categoria() {
+	}
+
 	public Categoria(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
